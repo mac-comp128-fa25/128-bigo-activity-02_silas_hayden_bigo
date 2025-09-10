7ebf0755c94cb8321fea58a026f0813deff0c69b
SilasRevenaugh <silasrevenaugh@gmail.com>
2025-09-10 18:11:12 -0500

    Finished All Necessary Parts For Full Credit

--- src/orderNotationActivity/BigODrill.java
+++ src/orderNotationActivity/BigODrill.java
@@ -12,11 +12,11 @@ public class BigODrill {
      * Demonstrates an algorithm with a constant run time, O(1)
      * @param numArr - an int array numArr
      * @return the last element of the array numArr
+     * Discussion: The runtime for this method will be the same regardless of 
+     * the amount of elements in the array.
      */
     public static int constantTime(int[] numArr) {
-        return numArr[numArr.length];
-        //TODO: Add the code that returns the last element of the input array with O(1) run time
-         // Placeholder to make it compile. Replace me with the correct value.
+        return numArr[numArr.length - 1];
     }
 
 
@@ -25,24 +25,36 @@ public class BigODrill {
      * Demonstrates an algorithm with a linear run time, O(n)
      * @param numArr - an integer array
      * @return the input array where each element of the array is multiplied with itself
+     * Discussion: The runtime will scale linearly with n. 
+     * Say each element in the array took 1 second to process(inaccurate).
+     * 1000 elements would take 1000 seconds, and one million elements would take one million seconds.
      */
     public static int[] linearTime(int[] numArr) {
         for (int i=0; i < numArr.length; i++) {
             numArr[i] = numArr[i] * numArr[i];
         }
-        //TODO: Add the code to modify and return the input array with each element squared
-        // which performs in O(n)
-
         return numArr;
     }
 
     /**
      * (3)
-     * Demonstrates an algorithm with TODO: What is O( ? ) of the following code?
+     * Demonstrates an algorithm with 
      * @param numArr - an integer array
+     * Discussion: The time complexity of this code is O(log n).
+     * A). We can tell that the runtime will be relatively quick at 1000
+     * and 1 million. This method's time complexity scales logarithmically,
+     * so as it gets larger, the increases in runtime will become fewer and fewer
+     * and will remain a constant duration.
+     * B). Because this function has a for loop, it is at least O(n).
+     * Within the for loop, it quadratically increases the rate at which it iterates through
+     * numArr. Because of this exponential increase in iteration jumps,
+     * we see a converse logarithmic increase in time complexity.
      */
     public static int[] puzzle03(int[] numArr) {
         //TODO: What does the following code do?
+        // The following code creates a new array the same size as the paramater array.
+        //It loops through the original array starting at index 1 and sets the values of 
+        //tempArray to numArr[2^index2]
         int index2 = 0;
         int[] tempArr = new int[numArr.length];
         for(int index = 1; index < numArr.length; index = index * 2) {
@@ -54,15 +66,32 @@ public class BigODrill {
 
     /**
      * (4)
-     * Demonstrates an algorithm with TODO: What is the O( ? ) of the following code?
+     * Demonstrates an algorithm with
      * @param numArr -  a two dimensional rectangular integer array a.k.a 2D matrix
      * @return a double that represents the sparsity of numArr
+     * The following code has a time complexity of O(n^2)
+     * A). The runtime of this method at n=1000 will be the amount of time each
+     * operation takes times 1000^2.
+     * The same will be true at n = one million, operation time times 1 million^2
+     * B).A real life example could be used by sports stadiums to track how many free t-shirts are caught
+     * out of the t-shirt cannon. The matrix could contain data about the number of t-shirts that
+     * each person in each row caught. The vast majority of people will not have caught any,
+     * making the matrix sparse.
+     * 
      */
     public static double sparsity(int[][] numArr) {
-        //TODO: Write the code that calculates and returns the sparsity of the input
-        // rectangular integer array numArr
-        return 0; // Placeholder to make it compile.
-
+        double zeroCount = 0;
+        double totalCount = 0;
+        for (int i = 0; i < numArr.length; i++) {
+            for (int j = 0; j < numArr[i].length; j++) {
+                totalCount+=1;
+                if (numArr[i][j] == 0) {
+                    zeroCount+=1;
+                }
+            }
+        }
+        double sparsityRatio = zeroCount/totalCount;
+        return sparsityRatio;
     }
 
     /**
